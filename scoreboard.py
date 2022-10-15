d28968d6693e78eac78db59feec3b5efa5ea3523
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-15 14:33:23 +0600

    Set value of high_score to 0 by reading from data.txt

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        # self.high_score = 0
+        with open("data.txt") as data: #opens file and reads and saves that value to high score***
+            self.high_score = int(data.read()) #***
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,7 +24,7 @@ class Scoreboard(Turtle):
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-    def reset(self):
+    def reset(self): #***
         if self.score > self.high_score:
             self.high_score = self.score
         self.score= 0 #if you put this above the if statement it will never ne triggered ***
