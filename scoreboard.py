ef00995463c5961066f7783bd7e881a9b632ae7c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-15 13:07:53 +0600

    Changed game_over function to reset function

--- scoreboard.py
+++ scoreboard.py
@@ -16,13 +16,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score= 0 #if you put this above the if statement it will never ne triggered ***
+        self.update_scoreboard()
+
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
